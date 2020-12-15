3dd2057e8b8e838b2c04b63ece58531f2309c861
Hridoy Roy <hridayroy05@gmail.com>
2020-12-15 18:04:42 +0600

    6:04

--- js/main.js
+++ js/main.js
@@ -134,3 +134,22 @@ var $slider = $(".slider"), $bullets = $(".bullets");
   
   
 	  }); // jquery end
+
+
+
+	  var swiper = new Swiper('.swiper-container', {
+		spaceBetween: 30,
+		centeredSlides: true,
+		autoplay: {
+		  delay: 2500,
+		  disableOnInteraction: false,
+		},
+		// pagination: {
+		//   el: '.swiper-pagination',
+		//   clickable: false,
+		// },
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	  });
